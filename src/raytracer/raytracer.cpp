d7a771d6c02538493270a8bc38b3ddd8ed8a641d
Jason Chen <kbtpodifo@gmail.com>
2022-12-07 18:30:49 -0500

    Parallelize rendering with QThreadPool
    Parallelize by spawning a task for every row of the image and handing it
    to the default thread pool.

--- src/raytracer/raytracer.cpp
+++ src/raytracer/raytracer.cpp
@@ -3,9 +3,9 @@
 #include "ray.h"
 #include "raytracescene.h"
 #include "utils/util.h"
+#include <QThreadPool>
 #include <iostream>
 #include <optional>
-#include <unordered_map>
 
 RayTracer::RayTracer(Config config) : m_config(config) {}
 
@@ -14,19 +14,25 @@ void RayTracer::render(RGBA* imageData, const RayTraceScene& scene) {
     auto width = scene.width();
     auto height = scene.height();
 
-    // Raytrace each pixel
-    for (int i = 0; i < width; i++) {
-        for (int j = 0; j < height; j++) {
-            auto ray = makeRay(i, j, width, height, camera);
+    // Spawn a task for every row of the image
+    for (int j = 0; j < height; j++) {
+        // Need to capture j by value, otherwise it may mutate before the task runs
+        auto task = QRunnable::create([&, j]() {
+            for (int i = 0; i < width; i++) {
+                auto ray = makeRay(i, j, width, height, camera);
 
-            auto intersection = intersectSDFShapes(ray, scene.getShapes());
+                auto intersection = intersectSDFShapes(ray, scene.getShapes());
 
-            if (intersection) {
-                auto directionToCamera =
-                    glm::normalize(camera.getPosition() - intersection->position);
-                imageData[j * width + i] =
-                    toRGBA(phong(*intersection, scene, directionToCamera, m_config));
+                if (intersection) {
+                    auto directionToCamera =
+                        glm::normalize(camera.getPosition() - intersection->position);
+                    imageData[j * width + i] =
+                        toRGBA(phong(*intersection, scene, directionToCamera, m_config));
+                }
             }
-        }
+        });
+        QThreadPool::globalInstance()->start(task);
     }
+
+    QThreadPool::globalInstance()->waitForDone();
 }
